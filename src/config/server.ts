b0f5ca5d49f257627d16109e7cf1065e8685f781
Roniel <roniel_ramos@hotmail.com>
2020-02-08 17:15:46 -0300

    feat: add configuracoes do servidor

---
+++ src/config/server.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+import cors, { CorsOptions } from 'cors'
+import helmet from 'helmet'
+import router from '../module/router'
+
+const app = express()
+
+const corsOptions: CorsOptions = {
+  allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'X-Requested-With'],
+  methods: ['POST', 'OPTIONS', 'GET'],
+  credentials: true,
+  origin: '*'
+}
+
+app.use(helmet())
+app.use(cors(corsOptions))
+app.use(express.json())
+
+app.get('/', (_req, res) => res.end())
+app.use(router)
+
+export default app

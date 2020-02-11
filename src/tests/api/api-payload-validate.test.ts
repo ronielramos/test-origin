93b89e33a38b4ba8cc55ca16c4a0650e2d89ccb7
Roniel <roniel_ramos@hotmail.com>
2020-02-11 00:25:34 -0300

    fix: alterar local de validação do codigo de status para eliminar falso positivo

--- src/tests/api/api-payload-validate.test.ts
+++ src/tests/api/api-payload-validate.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest'
 import app from '../../config/server'
+import router from '../../risk-profile/risk-profile.controller'
 
 const ENDPOINT = '/risk/profile'
 
@@ -14,59 +15,75 @@ const input = {
 }
 
 describe('Payload validation', () => {
-  test(`[POST] ${ENDPOINT} - Should return an error`, (done) => {
+  beforeAll(() => app.use(router))
+
+  test(`[POST] ${ENDPOINT} - Should return an error`, (done) =>
     request(app)
       .post(ENDPOINT)
       .send({})
-      .expect(400)
-      .end(() => done())
-  })
+      .end((_err, response) => {
+        expect(response.status).toBe(400)
+        done()
+      })
+  )
 
-  test(`[POST] ${ENDPOINT} - Should return an error with risk_questions`, (done) => {
+  test(`[POST] ${ENDPOINT} - Should return an error with risk_questions`, (done) =>
     request(app)
       .post(ENDPOINT)
       .send({ ...input, risk_questions: ['1', 'false', 'true'] })
-      .expect(400)
-      .end(() => done())
-  })
+      .end((_err, response) => {
+        expect(response.status).toBe(400)
+        done()
+      })
+  )
 
-  test(`[POST] ${ENDPOINT} - Should return an error with marital_status`, (done) => {
-    return request(app)
+  test(`[POST] ${ENDPOINT} - Should return an error with marital_status`, (done) =>
+    request(app)
       .post(ENDPOINT)
       .send({ ...input, marital_status: 'divorced' })
-      .expect(400)
-      .end(() => done())
-  })
+      .end((_err, response) => {
+        expect(response.status).toBe(400)
+        done()
+      })
+  )
 
-  test(`[POST] ${ENDPOINT} - Should return an error with house ownership_status`, (done) => {
-    return request(app)
+  test(`[POST] ${ENDPOINT} - Should return an error with house ownership_status`, (done) =>
+    request(app)
       .post(ENDPOINT)
       .send({ ...input, house: { ownership_status: 'unknown' } })
-      .expect(400)
-      .end(() => done())
-  })
+      .end((_err, response) => {
+        expect(response.status).toBe(400)
+        done()
+      })
+  )
 
-  test(`[POST] ${ENDPOINT} - Should return an error with income`, (done) => {
-    return request(app)
+  test(`[POST] ${ENDPOINT} - Should return an error with income`, (done) =>
+    request(app)
       .post(ENDPOINT)
       .send({ ...input, income: -50 })
-      .expect(400)
-      .end(() => done())
-  })
+      .end((_err, response) => {
+        expect(response.status).toBe(400)
+        done()
+      })
+  )
 
-  test(`[POST] ${ENDPOINT} - Should return an error with house`, (done) => {
-    return request(app)
+  test(`[POST] ${ENDPOINT} - Should return an error with house`, (done) =>
+    request(app)
       .post(ENDPOINT)
       .send({ ...input, house: {} })
-      .expect(400)
-      .end(() => done())
-  })
+      .end((_err, response) => {
+        expect(response.status).toBe(400)
+        done()
+      })
+  )
 
-  test(`[POST] ${ENDPOINT} - Should return an error with vehicle`, (done) => {
-    return request(app)
+  test(`[POST] ${ENDPOINT} - Should return an error with vehicle`, (done) =>
+    request(app)
       .post(ENDPOINT)
       .send({ ...input, vehicle: {} })
-      .expect(400)
-      .end(() => done())
-  })
+      .end((_err, response) => {
+        expect(response.status).toBe(400)
+        done()
+      })
+  )
 })
